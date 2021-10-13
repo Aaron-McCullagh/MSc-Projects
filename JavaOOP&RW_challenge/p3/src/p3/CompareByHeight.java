19f142e225741112f51e452738398ffd22313c8b
Aaron-McCullagh <83821213+Aaron-McCullagh@users.noreply.github.com>
2021-10-13 12:05:43 +0100

    Update CompareByHeight.java

--- JavaOOP&RW_challenge/p3/src/p3/CompareByHeight.java
+++ JavaOOP&RW_challenge/p3/src/p3/CompareByHeight.java
@@ -4,8 +4,7 @@ import java.util.Comparator;
 
 /**
  * Compares by height ; note use of complex seelction due to double (datatype) for height
- * @author Aaron
- *
+ * 
  */
 public class CompareByHeight implements Comparator<Player>{
 
